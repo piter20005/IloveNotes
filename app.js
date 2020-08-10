746675ce6976a56077391e99d3f92ed8edea4937
System Administrator <root@MacBook-Pro-Piotr.local>
2020-08-10 10:58:09 +0200

    Comments for better understanding the code

--- app.js
+++ app.js
@@ -26,20 +26,29 @@ ClassicEditor
         console.error(error);
     });
 
+
+//Get elements from DOM
 const plusButton = document.getElementsByClassName('plus-button')[0];
 const xButton = document.getElementsByClassName('x-button')[0];
 const addButton = document.getElementsByClassName('add-button')[0];
 
+
+//Event Listeners
 plusButton.addEventListener('click', () => showAndHideModal(true));
 xButton.addEventListener('click', () => showAndHideModal(false));
 addButton.addEventListener('click', (e) => {
+    //Get value from title input in modal
     const titleInpute = document.getElementById('title').value;
+    //Prevent from refreshing the page
     e.preventDefault()
+    //Get note from CKEditor
     const editorData = editor.getData();
+    //Fire up adding to Firebase
     addToFirebase(editorData, titleInpute, "piter");
-    // console.log(editorData)
 })
 
+
+//Shows and hides Modal
 function showAndHideModal(show) {
     const modalContainer = document.getElementsByClassName('modal-container')[0];
     if (show) {
@@ -50,6 +59,7 @@ function showAndHideModal(show) {
 
 }
 
+//Is fired up from addToFirebase, adds note and title from Firebase to the grid element on the site
 function addNote(note, title) {
     const gridElement = document.getElementsByClassName('notes-grid')[0];
     const newDiv = document.createElement('div');
@@ -58,12 +68,16 @@ function addNote(note, title) {
     gridElement.appendChild(newDiv);
 }
 
+//Takes parameters and creates new object in database
 function addToFirebase(note, title, username) {
+    // Gets reference to the database 
     let ref = firebase.database().ref(`users/${username}`)
+    // Pushes new object to the database with provided title and note
     ref.push({
         [title]: {
             note: note
         }
+        //Gets the key of new object and takes from it title and note and then fires up addNote with parameters
     }).then((snap) => {
         const key = snap.key;
         firebase.database().ref(`users/${username}/${key}`).on("value", snapshot => {
